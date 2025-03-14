3c07d118fb4d9ca6e0af8e5b126f76b7ac52f5b0
bambik <michal.olczak1515@gmail.com>
2025-03-14 17:25:05 +0100

    done internationalization

---
+++ src/main/java/pl/dmcs/rkotas/configuration/SpringConfiguration.java
@@ -0,0 +1,59 @@
+package pl.dmcs.rkotas.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("pl.dmcs.rkotas")
+public class SpringConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    // Configure message source directory
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("/resources/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    // Configure LocaleResolver with default locale as 'en'
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(new Locale("en"));
+        resolver.setCookieName("myLocaleCookie");
+        resolver.setCookieMaxAge(4800);
+        return resolver;
+    }
+
+    // Configure interceptor to switch language when 'lang' parameter found in request
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        registry.addInterceptor(interceptor);
+    }
+
+}
+
---
+++ src/main/webapp/appUser.jsp
@@ -0,0 +1,46 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<html>
+<head>
+    <title><spring:message code="label.addAppUser"/></title>
+</head>
+<body>
+
+<div class="header">
+    <span style="float: right">
+    	<a style="background-color: white" href="?lang=pl">pl</a> | <a style="background-color: white" href="?lang=en">en</a> | <a style="background-color: white" href="?lang=de">de</a>
+	</span>
+</div>
+
+<h1>App user info:</h1>
+<form:form method="post" action="addAppUser" modelAttribute="appUser">
+
+    <table>
+        <tr>
+            <td><form:label path="firstName"><spring:message code="label.firstName"/></form:label></td>
+            <td><form:input path="firstName" /></td>
+        </tr>
+        <tr>
+            <td><form:label path="lastName"><spring:message code="label.lastName"/></form:label></td>
+            <td><form:input path="lastName" /></td>
+        </tr>
+        <tr>
+            <td><form:label path="email"><spring:message code="label.email"/></form:label></td>
+            <td><form:input path="email" /></td>
+        </tr>
+        <tr>
+            <td><form:label path="telephone"><spring:message code="label.telephone"/></form:label></td>
+            <td><form:input path="telephone" /></td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <input type="submit" value="<spring:message code="label.addAppUser"/>"/>
+            </td>
+        </tr>
+    </table>
+
+</form:form>
+</body>
+</html>
+
